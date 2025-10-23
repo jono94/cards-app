4e5fc88fd457c38d8051902318cf7a3b824753ff
Jono Delbridge <jonodel949@gmail.com>
2025-10-24 09:49:51 +1100

    Setup Nativewind.

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*{html,js,jsx,ts,tsx,mdx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
