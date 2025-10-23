1d29cbb82201dd48daa0f7e540469c4ff380771d
Jono Delbridge <jonodel949@gmail.com>
2025-10-24 09:30:07 +1100

    Swap from single quote to double quote as this seems to be the standard across projects.

--- frontend/src/app/(tabs)/_layout.tsx
+++ frontend/src/app/(tabs)/_layout.tsx
@@ -1,8 +1,8 @@
-import { Tabs } from 'expo-router';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import Entypo from '@expo/vector-icons/Entypo';
-import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { Tabs } from "expo-router";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import Entypo from "@expo/vector-icons/Entypo";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
 export default function TabLayout() {
   return (
@@ -10,14 +10,14 @@ export default function TabLayout() {
       <Tabs.Screen
         name="home"
         options={{
-          title: 'Home',
+          title: "Home",
           tabBarIcon: ({ color, size }) => <FontAwesome name="home" size={size} color={color} />,
         }}
       />
       <Tabs.Screen
         name="received-cards"
         options={{
-          title: 'Received Cards',
+          title: "Received Cards",
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="email-open-heart-outline" size={size} color={color} />
           ),
@@ -26,14 +26,14 @@ export default function TabLayout() {
       <Tabs.Screen
         name="card-creator"
         options={{
-          title: 'Card Creator',
+          title: "Card Creator",
           tabBarIcon: ({ color, size }) => <Entypo name="new-message" size={size} color={color} />,
         }}
       />
       <Tabs.Screen
         name="template-gallery"
         options={{
-          title: 'Template Gallery',
+          title: "Template Gallery",
           tabBarIcon: ({ color, size }) => (
             <FontAwesome5 name="palette" size={size} color={color} />
           ),
