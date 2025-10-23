19d0cfefeb2e50ba438a56ee85c3ec725d1e9764
Jono Delbridge <jonodel949@gmail.com>
2025-10-24 10:01:06 +1100

    Setup React Native Reusables.

--- frontend/src/app/_layout.tsx
+++ frontend/src/app/_layout.tsx
@@ -1,11 +1,15 @@
 import "../styles/global.css";
 import { Stack } from "expo-router";
+import { PortalHost } from "@rn-primitives/portal";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <>
+      <Stack>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+      <PortalHost />
+    </>
   );
 }
