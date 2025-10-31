4bca3e629b41cf92ccb527917dcff0141f21445c
Jono Delbridge <jonodel949@gmail.com>
2025-10-31 12:09:11 +1100

    Move tab sections to their on Stack navigation layers. Also test out Expo EAS updates functions!

---
+++ frontend/src/app/(tabs)/home/_layout.tsx
@@ -0,0 +1,22 @@
+import { Stack } from "expo-router";
+import { Platform } from "react-native";
+import SettingsPopover from "@/src/components/settings/settingsPopover";
+import { useTranslation } from "react-i18next";
+
+// Ensure deep links push the "index.tsx" route onto the stack first
+export const unstable_settings = {
+  initialRouteName: "index",
+};
+
+export default function TemplateGalleryLayout() {
+  const { t } = useTranslation();
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: Platform.OS === "web" ? false : true,
+        headerTitle: t("navigation.home"),
+        headerRight: () => <SettingsPopover />,
+      }}
+    />
+  );
+}
