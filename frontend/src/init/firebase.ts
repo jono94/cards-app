f31431339398ccc44e7698c357b1462c1bc90a50
Jono Delbridge <jonodel949@gmail.com>
2025-11-07 12:41:50 +1100

    Add firebase state persistence on mobile.

--- frontend/src/init/firebase.ts
+++ frontend/src/init/firebase.ts
@@ -1,6 +1,14 @@
+import { Platform } from "react-native";
 import { settings } from "@/src/lib/settings";
-import { getAuth, connectAuthEmulator } from "firebase/auth";
+import {
+  getAuth,
+  connectAuthEmulator,
+  initializeAuth,
+  // @ts-ignore: This is valid by raises an error no exported member
+  getReactNativePersistence,
+} from "firebase/auth";
 import { initializeApp } from "firebase/app";
+import { getItem, setItem, removeItem } from "@/src/lib/localStorage";
 
 const firebaseConfig = {
   apiKey: "fake-api-key",
@@ -13,6 +21,18 @@ const firebaseConfig = {
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
+
+// https://github.com/expo/fyi/blob/main/firebase-js-auth-setup.md
+if (Platform.OS !== "web") {
+  initializeAuth(firebaseApp, {
+    persistence: getReactNativePersistence({
+      getItem: (key: string) => getItem(key),
+      setItem: (key: string, value: string) => setItem(key, value),
+      removeItem: (key: string) => removeItem(key),
+    }),
+  });
+}
+
 const auth = getAuth(firebaseApp);
 if (settings.firebaseAuthEmulatorUrl) {
   connectAuthEmulator(auth, settings.firebaseAuthEmulatorUrl);
